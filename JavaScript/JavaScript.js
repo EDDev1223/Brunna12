4ea19b1dc944fb91b73699b4ea597612f6972530
Edemilson Machado Viana Junior <edemilsonjr_sbc@hotmail.com>
2024-08-18 11:29:00 -0300

    Update Brunna

---
+++ JavaScript/JavaScript.js
@@ -0,0 +1,60 @@
+// script.js
+document.getElementById('sobremim').addEventListener('click', function() {
+    // Define a altura para rolar (em pixels)
+    const scrollToHeight = 800;
+
+    // Rolagem suave até a altura desejada
+    window.scrollTo({
+        top: scrollToHeight,
+        behavior: 'smooth'
+    });
+});
+
+// script.js
+document.getElementById('atendimento').addEventListener('click', function() {
+    // Define a altura para rolar (em pixels)
+    const scrollToHeight = 800;
+
+    // Rolagem suave até a altura desejada
+    window.scrollTo({
+        top: scrollToHeight,
+        behavior: 'smooth'
+    });
+});
+
+// script.js
+document.getElementById('clinica').addEventListener('click', function() {
+    // Define a altura para rolar (em pixels)
+    const scrollToHeight = 800;
+
+    // Rolagem suave até a altura desejada
+    window.scrollTo({
+        top: scrollToHeight,
+        behavior: 'smooth'
+    });
+});
+
+// script.js
+document.getElementById('consultorio').addEventListener('click', function() {
+    // Define a altura para rolar (em pixels)
+    const scrollToHeight = 800;
+
+    // Rolagem suave até a altura desejada
+    window.scrollTo({
+        top: scrollToHeight,
+        behavior: 'smooth'
+    });
+});
+
+// script.js
+document.getElementById('contato').addEventListener('click', function() {
+    // Define a altura para rolar (em pixels)
+    const scrollToHeight = 800;
+
+    // Rolagem suave até a altura desejada
+    window.scrollTo({
+        top: scrollToHeight,
+        behavior: 'smooth'
+    });
+});
+
